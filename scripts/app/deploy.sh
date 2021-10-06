1168e9f15ab1dc5e408fe7134bbe57409bbadd3a
jaylordibe <jaylordibe@gmail.com>
2021-10-06 19:12:43 +0800

    Updated app deployment script

--- scripts/app/deploy.sh
+++ scripts/app/deploy.sh
@@ -15,6 +15,5 @@ ng build --configuration=staging
 "
 docker exec -t -w /var/www/html app-service bash -c "$COMMANDS"
 sudo mv dist/app .
-sudo rm -r dist
-sudo mv app dist
+sudo mv app public
 sudo cp ${WORKING_DIRECTORY}/.htaccess dist/
